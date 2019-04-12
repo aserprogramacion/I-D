1d81d80c5fe76029fba63e0ea92e890ebb5d07e8
usuario <usuario@172.16.2.164>
2019-04-12 17:23:54 +0200

    Introducida la funcionalidad 2

--- src/i/d/ID.java
+++ src/i/d/ID.java
@@ -4,8 +4,8 @@ package i.d;
 public class ID {
 
     public static void main(String[] args) {
-        System.out.println("Modificacion 1");
         System.out.println("Modificacion 2");
+        System.out.println("Funcionalidad 2");
     }
     
 }
